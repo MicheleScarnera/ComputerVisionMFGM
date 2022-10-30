603d74d1f01188e9c1003934e2d696d876590835
MicheleScarnera <115538003+MicheleScarnera@users.noreply.github.com>
2022-10-30 22:09:58 +0100

    me banging my head until images are actually downloaded

--- import_imaterialist.py
+++ import_imaterialist.py
@@ -1,6 +1,12 @@
+import numpy as np
 import pandas as pd
+import tensorflow as tf
+import matplotlib as plt
+import imageio.v3 as iio
+import requests
 import json
-
+from io import BytesIO
+import time
 
 def get_task_map() -> dict[int, str]:
     taskMapPath = "iMaterialist/fgvc4_iMat.task_map.json"
@@ -35,7 +41,54 @@ def get_apparel_class_map(task_map = None) -> dict[int, int]:
     return result
 
 
-def import_data():
+def get_image_file(url_list, image_size = 256):
+    if type(url_list) is str:
+        url_list = [url_list]
+
+    print(f"Looking for working URL...")
+    for url in url_list:
+        print(f"Trying URL {url}...")
+        time.sleep(0.1)
+        try:
+            res = requests.get(url, stream=True, timeout=5)
+
+        except requests.exceptions.ConnectionError:
+            print(f"Connection Error")
+            continue
+        except requests.exceptions.ReadTimeout:
+            print("Timed out")
+            continue
+        except OSError:
+            print(f"OSError")
+            continue
+        except ValueError:
+            print(f"Value Error")
+            continue
+
+        try:
+            if res.ok:
+                img_arr = iio.imread(BytesIO(res.content))
+
+                img_arr = tf.image.resize(images=img_arr, size=(image_size, image_size)
+                                          , method='bicubic', antialias=True)
+
+                img_arr = img_arr / 255
+
+                print(f"Image retrieved successfully")
+                return img_arr
+            else:
+                continue
+        except OSError:
+            print(f"OSError")
+            continue
+        except ValueError:
+            print(f"Value Error")
+            continue
+
+    print("No image found")
+    return np.empty(shape=(1, 1))
+
+def import_data(small_dataset = True, verbose = False):
     trainingPath = "iMaterialist/fgvc4_iMat.train.data.json"
 
     with open(trainingPath, 'r') as f:
@@ -46,15 +99,25 @@ def import_data():
     df_images = pd.json_normalize(data, "images", ["url", "imageId"],
                                   record_prefix='_', errors='ignore')
 
-    print(f"Printing df_images...\n{df_images}")
+    print("Image IDs retrieved")
+    if verbose: print(f"Printing df_images...\n{df_images}")
 
     df_annotations = pd.json_normalize(data, "annotations", ["labelId", "imageId", "taskId"],
                                        record_prefix='_', errors='ignore')
 
-    print(f"Printing df_annotations...\n{df_annotations}")
+    print("Image annotations retrieved")
+    if verbose: print(f"Printing df_annotations...\n{df_annotations}")
 
     df = pd.merge(df_images, df_annotations, on="_imageId")
     df = df[['_imageId', '_url', '_taskId', '_labelId']]
+
+    #print(type(df.loc[0, '_url']))
+    print("Image IDs and annotations merged")
+
+    if small_dataset:
+        l = 5
+        print(f"Dataset size reduced to {l}")
+        df = df[0:l]
 
     # remove the underscores from the column names
     df.rename(
@@ -74,13 +137,22 @@ def import_data():
     # - create a new variable for the apparel class (each taskid belongs to one and only one apparel class)
     # - download the image from the first URL that's responsive
 
+    # apparel class
     apparel_class_map = get_apparel_class_map()
     df['apparel_class'] = df['taskId'].apply(lambda x: apparel_class_map[x])
 
-    print(f"Printing df...\n{df}")
+    # download images
+    print("Downloading images...")
+    imagedata_out = list()
+    for index, row in df.iterrows():
+        imagedata_out.append(get_image_file(row['url']))
+
+    print("Images downloaded")
+    print(type(imagedata_out))
+    print(imagedata_out)
+
+    if verbose: print(f"Printing df...\n{df}")
     return df
-
-
 
 Data = import_data()
 
